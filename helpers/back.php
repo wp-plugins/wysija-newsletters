c7696fee887f060dd0529de3ff241e197c23cca2
wysija <wysija@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-08 07:29:04 +0000

    Applied security fix on old versions of Wysija / MailPoet
    git-svn-id: https://plugins.svn.wordpress.org/wysija-newsletters/tags/2.6@944747 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- helpers/back.php
+++ helpers/back.php
@@ -46,6 +46,7 @@ class WYSIJA_help_back extends WYSIJA_help{
 
         }else{
             if(WYSIJA_ITF)  {
+                add_action('admin_init', array( $this , 'verify_capability'),1);
                 add_action('admin_init', array($this->controller, 'main'));
                 add_action('after_setup_theme',array($this,'resolveConflicts'));
             }
@@ -74,6 +75,43 @@ class WYSIJA_help_back extends WYSIJA_help{
         //if the comment form option is activated then we add an approval action
         if($config->getValue('commentform')){
             add_action('wp_set_comment_status', array($this,'comment_approved'), 60,2);
+        }
+    }
+
+    /**
+     * this function will check the role of the user executing the action, if it's called from another
+     * WordPress admin page than page.php for instance admin-post.php
+     * @return boolean
+     */
+    function verify_capability(){
+        if( isset( $_GET['page'] ) && substr( $_GET['page'] ,0 ,7 ) == 'wysija_' ){
+
+            switch( $_GET['page'] ){
+                case 'wysija_campaigns':
+                    $role_needed = 'wysija_newsletters';
+                    break;
+                case 'wysija_subscribers':
+                    $role_needed = 'wysija_subscribers';
+                    break;
+                case 'wysija_config':
+                    $role_needed = 'wysija_config';
+                    break;
+                case 'wysija_statistics':
+                    $role_needed = 'wysija_stats_dashboard';
+                    break;
+                default:
+                    $role_needed = 'switch_themes';
+            }
+
+            if( current_user_can( $role_needed ) ){
+                return true;
+            } else{
+                die( 'You are not allowed here.' );
+            }
+
+        }else{
+            // this is not a wysija interface/action we can let it pass
+            return true;
         }
     }
 
